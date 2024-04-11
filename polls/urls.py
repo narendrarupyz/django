37c6e4a99575618ad025194fff6f67228b70d28a
Narendra Kumar <narendra.kumar@rupyz.com>
2024-04-11 18:44:42 +0530

    updated amend url using genric

--- polls/urls.py
+++ polls/urls.py
@@ -4,11 +4,17 @@ from . import views
 app_name = 'polls'
 
 urlpatterns =[
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     #path('<int:question_id>/', views.detail, name='detail'),
     # added the word 'specifics'
     #path('specifics/<int:question_id>/', views.detail, name='detail'),
-    path('<int:q_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results', views.results, name='results'),
-    path('<int:question_id>/vote', views.vote, name='vote')
+    #path('<int:question_id>/', views.detail, name='detail'),
+    #path('<int:question_id>/results', views.results, name='results'),
+    #path('<int:question_id>/vote', views.vote, name='vote')
+
+    #By using genric views
+    path('',views.IndexViews.as_view(), name='index'),
+    path('<int:pk>/',views.DetaliView.as_view(), name='detail'),
+    path('<int:pk>/results/',views.Results.as_view(), name='results'),
+    path('<int:question_id>/vote/',views.vote, name='vote'),
     ]
